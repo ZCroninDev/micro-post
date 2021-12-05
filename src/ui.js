ee83c70b02571f1c6ca24d0175590bcb42274de6
zach <ZCroninDev@users.noreply.github.com>
2021-12-05 17:50:23 -0500

    Get and display post

--- src/ui.js
+++ src/ui.js
@@ -7,4 +7,28 @@ class UI {
     this.postSubmit = document.querySelector('.post-submit');
     this.forState = document.querySelector('add');
   }
+
+  showPost(posts) {
+    let output = '';
+
+    post.forEach((post) => {
+      output += `
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="${post.title}"</h4>
+            <p class="card-text">${post.title}</h4>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+            <i class="fa fa-pencil"</i>
+            </a>
+
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"</i>
+            </a>
+          </div>
+        </div>
+        `;
+    });
+  }
 }
+
+export const ui = new UI();
